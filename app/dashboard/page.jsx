b4879f0716781637a40e937f80682b819257c586
zikran69 <zikzigi69@gmail.com>
2023-12-24 19:52:09 +0700

    add card and transaction component

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import styles from "../ui/dashboard/dashboard.module.css";
+import Card from "../ui/dashboard/card/card";
+import Rightbar from "../ui/dashboard/rightbar/rightbar";
+import Transaction from "../ui/dashboard/transaction/transaction";
+import Chart from "../ui/dashboard/chart/chart";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transaction />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
