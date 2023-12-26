8481150758002063398d4150807efbb458e4239e
zikran69 <zikzigi69@gmail.com>
2023-12-26 23:13:40 +0700

    add page to add new user admin & product

---
+++ app/dashboard/products/add/page.jsx
@@ -0,0 +1,30 @@
+import styles from "@/app/ui/dashboard/products/addProduct/addProduct.module.css";
+
+const AddProductPage = () => {
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <input type="text" placeholder="title" name="title" required />
+        <select name="cat" id="cat">
+          <option value="general">Pilih Category</option>
+          <option value="foods">Makanan</option>
+          <option value="drinks">Minuman</option>
+          <option value="snack">Camilan</option>
+        </select>
+        <input type="number" placeholder="price" name="price" />
+        <input type="number" placeholder="stock" name="stock" />
+        <input type="text" placeholder="taste" name="taste" />
+        <input type="text" placeholder="size" name="size" />
+        <textarea
+          name="desc"
+          id="desc"
+          rows="16"
+          placeholder="Description"
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddProductPage;
