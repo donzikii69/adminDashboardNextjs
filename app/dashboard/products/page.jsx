7b6f3f3822bfc155829f27d381cea90b0163fff8
zikran69 <zikzigi69@gmail.com>
2023-12-27 18:40:32 +0700

    add view page user

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -44,7 +44,7 @@ const ProductPage = () => {
             <td>11</td>
             <td>
               <div className={styles.buttons}>
-                <Link href="/">
+                <Link href="/dashboard/products/test">
                   <button className={`${styles.button} ${styles.view}`}>
                     View
                   </button>
---
+++ app/dashboard/users/[id]/page.jsx
@@ -0,0 +1,40 @@
+import styles from "@/app/ui/dashboard/users/singleUser/singleUser.module.css";
+import Image from "next/image";
+const SingleUserPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.imgContainer}>
+          <Image src="/noavatar.png" alt="" fill />
+        </div>
+        zikran
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <label>Username</label>
+          <input type="text" name="username" placeholder="zikran" />
+          <label>Email</label>
+          <input type="email" name="email" placeholder="zikran@gmail.com" />
+          <label>Password</label>
+          <input type="password" name="password" placeholder="zikran" />
+          <label>Phone</label>
+          <input type="text" name="phone" placeholder="+123456789" />
+          <label>Address</label>
+          <textarea type="text" name="address" placeholder="Jakarta" />
+          <label>Is Admin?</label>
+          <select name="isAdmin" id="isAdmin">
+            <option value={true}>Yes</option>
+            <option value={false}>No</option>
+          </select>
+          <label>Is Active?</label>
+          <select name="isActive" id="isActive">
+            <option value={true}>Yes</option>
+            <option value={false}>No</option>
+          </select>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SingleUserPage;
